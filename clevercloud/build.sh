7be65d5300f2b15dd197c65e1460cf289c937e4b
Aurelien Meunier <me@aurelienmeunier.com>
2023-04-12 12:36:40 +0200

    chore: Just to notify a new deploy on CC

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Getting izanami jar"
-echo "wget 'https://github.com/MAIF/izanami/releases/latest/download/izanami.jar' -P ${APP_HOME}"
+echo "Getting izanami jar: wget 'https://github.com/MAIF/izanami/releases/latest/download/izanami.jar' -P ${APP_HOME}"
 
 wget 'https://github.com/MAIF/izanami/releases/latest/download/izanami.jar' -P ${APP_HOME}
 
