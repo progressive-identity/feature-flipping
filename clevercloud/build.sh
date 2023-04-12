6ce2a027e82b35e0afd8ec7a240c115a00e6ad34
Aurelien Meunier <me@aurelienmeunier.com>
2023-04-12 12:15:09 +0200

    config: Update url of the latest JAR

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Getting izanami jar"
-echo "wget 'https://dl.bintray.com/maif/binaries/izanami.jar/latest/izanami.jar' -P ${APP_HOME}"
+echo "wget 'https://github.com/MAIF/izanami/releases/latest/download/izanami.jar' -P ${APP_HOME}"
 
-wget 'https://dl.bintray.com/maif/binaries/izanami.jar/latest/izanami.jar' -P ${APP_HOME}
+wget 'https://github.com/MAIF/izanami/releases/latest/download/izanami.jar' -P ${APP_HOME}
 
 
