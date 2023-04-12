affcbc6f34f2674f14c9b281f18dc68fd6d0ac2e
Aurelien Meunier <me@aurelienmeunier.com>
2023-04-12 12:13:31 +0200

    config: Add logs to build

--- clevercloud/build.sh
+++ clevercloud/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Getting izanami jar"
+echo "wget 'https://dl.bintray.com/maif/binaries/izanami.jar/latest/izanami.jar' -P ${APP_HOME}"
 
-wget --quiet 'https://dl.bintray.com/maif/binaries/izanami.jar/latest/izanami.jar' -P ${APP_HOME}
+wget 'https://dl.bintray.com/maif/binaries/izanami.jar/latest/izanami.jar' -P ${APP_HOME}
 
 
